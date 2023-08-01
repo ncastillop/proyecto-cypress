463162aad35d940a6733145b89eb2217a0fec9ec
Nicolas <castillonicolas1194@gmail.com>
2023-08-01 00:45:23 -0400

    Creación de archivos place order page

---
+++ cypress/e2e/pages/placeOrder/PlacceOrder.data.js
@@ -0,0 +1,13 @@
+export class PlacceOrderData{
+    static get testData(){
+        return{
+            name: 'Nicolas',
+            country:'Chile',
+            city:'Santiago',
+            month: 12,
+            creditCard: 378282246310005,
+            year: 2027
+
+        }
+    }
+}
---
+++ cypress/e2e/pages/placeOrder/PlaceOrder.methods.js
@@ -0,0 +1,37 @@
+import { PlaceOrderElements } from "./PlaceOrder.elements";
+
+export class PlaceOrderMethods{
+
+    static insertName(name){
+        PlaceOrderElements.textBoxes.name.invoke('val',name)
+    }
+
+    static insertCountry(country){
+        PlaceOrderElements.textBoxes.country.invoke('val',country)
+    }
+
+    static insertCity(city){
+        PlaceOrderElements.textBoxes.city.invoke('val',city)
+    }
+
+    static insertCreditCard(creditCard){
+        PlaceOrderElements.textBoxes.creditCard.invoke('val',creditCard)
+    }
+
+    static insertMonth(month){
+        PlaceOrderElements.textBoxes.month.invoke('val',month)
+    }
+
+    static insertYear(year){
+        PlaceOrderElements.textBoxes.year.invoke('val',year)
+    }
+
+    static clickOnCloseButton(){
+        PlaceOrderElements.buttons.close.click()
+    }
+
+    static clickOnPurchaseButton(){
+        PlaceOrderElements.buttons.purchase.click()
+    }
+
+}
