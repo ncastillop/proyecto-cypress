dbd2ee7b12612ba13d28f0b98e47c0464ee53302
Nicolas <castillonicolas1194@gmail.com>
2023-08-01 19:36:22 -0400

    CP: Registro invalido

--- cypress/e2e/tests/SignupAndLogin.cy.js
+++ cypress/e2e/tests/SignupAndLogin.cy.js
@@ -3,6 +3,9 @@ import { CommonPageMethods } from "../pages/common-page/CommonPage.methods"
 import { SignupMethods } from "../pages/signup/Signup.methods"
 import { Logger } from "../util/Logger"
 
+const user = CommonPageMethods.generateRandomString()
+const pass = CommonPageMethods.generateRandomString(7)
+
 describe(CommonPageData.testSuites.registroYAutenticacion, ()=>{
     it('Registro de usuario valido',()=>{
 
@@ -16,8 +19,8 @@ describe(CommonPageData.testSuites.registroYAutenticacion, ()=>{
         //cy.wait(30000)
         Logger.stepNumber(3)
         Logger.stepLog('Completar todos los campos obligatorios con informacion valida')
-        SignupMethods.insertUsername('Nicolas001wq')
-        SignupMethods.insertPassword('Nicolas001')
+        SignupMethods.insertUsername(user)
+        SignupMethods.insertPassword(pass)
 
         Logger.stepNumber(4)
         Logger.stepLog('Hacer clic en "Sign up" para registrar el usuario')
@@ -25,6 +28,29 @@ describe(CommonPageData.testSuites.registroYAutenticacion, ()=>{
         cy.wait(3000)
         Logger.verification('Verificar que se muestre el mensaje "Sign up successful"')
         SignupMethods.verifySignupSuccesfulMessageIsDisplayed()
+
+    });
+    it('Registro de usuario invalido',()=>{
+
+        Logger.stepNumber(1)
+        Logger.stepLog('Navegar a la pagina de inicio')
+        CommonPageMethods.navigateToDemoBlaze()
+
+        Logger.stepNumber(2)
+        Logger.stepLog('Hacer clic en "Sign up" en la barra de navegacion')
+        CommonPageMethods.clickOnSignUpOption()
+        //cy.wait(30000)
+        Logger.stepNumber(3)
+        Logger.stepLog('Completar todos los campos obligatorios con informacion invalida')
+        SignupMethods.insertUsername('random01')
+        SignupMethods.insertPassword('random01')
+
+        Logger.stepNumber(4)
+        Logger.stepLog('Hacer clic en "Sign up" para registrar el usuario')
+        SignupMethods.clickOnSignupButton()
+        cy.wait(3000)
+        Logger.verification('Verificar que se muestre el mensaje "This user already exist."')
+        SignupMethods.verifySignupErrorMessageIsDisplayed()
 
     })
 })
